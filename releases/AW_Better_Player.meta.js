17a886d7178af42dca8eabf837269c4cb19f4b61
KeyMan98 <78966959+KeyMan98@users.noreply.github.com>
2026-08-29 20:38:07 +0200

    release attuale

---
+++ releases/AW_Better_Player.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         AnimeWorld Better Player
+// @namespace    aw-better-player
+// @version      3.0.0
+// @match        *://www.animeworld.ac/play/*
+// @run-at       document-start
+// @description  Il player migliore di sempre — riscritto da zero.
+// @description:it Il player migliore di sempre — riscritto da zero.
+// @license      MIT
+// @grant        unsafeWindow
+// @connect      cibernetic-gg.workers.dev
+// @updateURL    https://raw.githubusercontent.com/KeyMan98/Animeworld-Better-Player/main/releases/AW_Better_Player.meta.js
+// @downloadURL  https://raw.githubusercontent.com/KeyMan98/Animeworld-Better-Player/main/releases/AW_Better_Player.user.js
+// ==/UserScript==
